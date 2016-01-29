2922eb2d9f8d45ba3cd47a544968fc50270b728a
VanHulleOne <vanhullel@my.uwstout.edu>
2016-01-29 16:11:03 -0600

    First Commit

---
+++ billards.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 15:37:59 2016
+
+@author: lvanhulle
+"""
+
+import numpy as np
+
+m, n = 3, 5
+pq = np.array([[1,1], [1,4]])
+
+table = np.array([[[0,0], [m,0]], [[m,0],[m,n]], [[m,n], [0,n]], [[0,n],[0,0]]])
+
+collisions = []
+travel = np.array([[0,0], [1,1]])
+
+
+
+
+        
+        
