582fa9b5af8248acf4479ec38120aff70c4859e0
Luke Home PC <beardedone85@gmail.com>
2016-01-31 18:41:08 -0600

    A little cleaning up

--- billards.py
+++ billards.py
@@ -8,46 +8,43 @@ Created on Fri Jan 29 15:37:59 2016
 import numpy as np
 import time
 
-m, n = 3, 4
+m, n = 2, 3
 pq = np.array([[1,1], [0,3]])
+POINT, VECT = 0, 1
+X, Y = 0,1
 
 #lines are of the form startPoint, line vector
-table = np.array([[[0,0], [m,0]], [[m,0],[0,n]], [[0,n], [m,0]], [[0,0],[0,n]]])
+table = np.array([[[0,0], [m,0]], [[m,0],[0,n]], [[m,n], [-m,0]], [[0,n],[0,-n]]])
 
-collisions = []
+collisions = [np.array([0.,0.])]
 travel = np.array([[0,0], [1,1]])
 
-for side in table:
-    t = np.cross((travel[0] - side[0]), travel[1])/(1.0*np.cross(side[1], (travel[1])))
-    print t
 
-#returns the scalar for the intersection on line2
-def crosser(line1, line2):
-    return np.cross((line1[0] - line2[0]), line1[1])/(1.0*np.cross(line2[1], (line1[1])))
-    
-def table_cross_gen():
-    for side in table:
-        yield crosser(travel, side)
+def getLineConst(line, cLine):
+    return (np.cross((line[POINT] - cLine[POINT]), line[VECT])/
+        (1.0*np.cross(cLine[VECT], (line[VECT]))))
+        
+def getTU(uLine, tLine):
+    return getLineConst(uLine, tLine), getLineConst(tLine, uLine)
 
 intersection = np.array([0,0])
-goal = np.array([m,n])
 
 epsilon = 0.001
 
 
-for j in range(m+n-1):
+for j in xrange(m+n-1):
     for i in xrange(len(table)):
-        t = crosser(travel, table[i])
-        u = crosser(table[i], travel)
+        t, u = getTU(travel, table[i])
         if t >= 0 and t <= 1 and u > 0:
-            intersection = table[i][0] + t * table[i][1]
-            print 'Intersection'            
-            print intersection
+            intersection = table[i][POINT] + t * table[i][VECT]
+            collisions.append(intersection)
             travel[0] = intersection
             if i%2:
-                travel[1][0] *= -1
+                travel[VECT][X] *= -1
             else:
-                travel[1][1] *= -1
-            print 'Travel'
-            print travel
-            time.sleep(.1)
+                travel[VECT][Y] *= -1
+
+assert(len(collisions) == m+n)
+assert(any(np.all(np.equal(line[POINT], (collisions[-1]))) for line in table))          
+for c in collisions:
+    print c
